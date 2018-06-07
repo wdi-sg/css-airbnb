1bf9e805854bf5d813751faf86b96340121cbb29
adna <adna@adna.com>
2018-06-07 12:57:31 +0800

    Add bonus answers

---
+++ script.js
@@ -0,0 +1,21 @@
+// service unavailable
+let seeAll = document.querySelectorAll(".see-all");
+
+let featureDown = function() {
+  alert("The feature is not available yet.");
+}
+
+seeAll.forEach(function(seeAllEach) {
+  seeAllEach.addEventListener("click", featureDown);
+})
+
+// select an array of all images
+var images = document.querySelectorAll(".card");
+
+let openLink = function() {
+  window.open("https://www.google.com");
+}
+
+images.forEach(function(image) {
+  image.addEventListener("click", openLink);
+})
